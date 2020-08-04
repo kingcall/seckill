794244dd6a20d3065e769d40a27e11a6e6aa888f
kingcall <kingcall@hotmail.com>
2020-08-04 23:08:58 +0800

    商品结构

---
+++ src/main/java/com/kingcall/seckill/entity/Item.java
@@ -0,0 +1,65 @@
+package com.kingcall.seckill.entity;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private Integer id;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private String description;
+
+    private Integer sales;
+
+    private String imgurl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl == null ? null : imgurl.trim();
+    }
+}
---
+++ src/main/java/com/kingcall/seckill/mapper/ItemStockMapper.java
@@ -0,0 +1,17 @@
+package com.kingcall.seckill.mapper;
+
+import com.kingcall.seckill.entity.ItemStock;
+
+public interface ItemStockMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ItemStock record);
+
+    int insertSelective(ItemStock record);
+
+    ItemStock selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ItemStock record);
+
+    int updateByPrimaryKey(ItemStock record);
+}
